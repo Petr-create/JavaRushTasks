03a582e52d5f8a8766e96d20bbcc17e21ce5280f
petr <petr2768@yandex.ru>
2020-09-08 20:18:33 +0300

    вторая попытка отправить tasks1822

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task18.task1822;
+
+/* 
+Поиск данных внутри файла
+*/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            throw new IllegalArgumentException();
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileReader fileReader = new FileReader(reader.readLine());
+        BufferedReader buff = new BufferedReader(fileReader);
+
+        String result;
+
+        while((result = buff.readLine()) != null){
+            if(result.startsWith(args[0] + " ")){
+                System.out.println(result);
+                break;
+            }
+        }
+        reader.close();
+        fileReader.close();
+        buff.close();
+    }
+}
