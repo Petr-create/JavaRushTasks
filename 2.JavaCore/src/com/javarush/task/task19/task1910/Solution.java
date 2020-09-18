4f38877d3f39a10b1ac3bc476fc879a59607ff40
petr <petr2768@yandex.ru>
2020-09-18 12:50:34 +0300

    task1910 Пунктуация

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task19.task1910;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/* 
+Пунктуация
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileIn = reader.readLine();
+        String fileOut = reader.readLine();
+        reader.close();
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileIn));
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileOut));
+        while (fileReader.ready()){
+            String str = fileReader.readLine();
+            str = str.replaceAll("\\p{Punct}\\n", " ");
+            fileWriter.write(str);
+            fileWriter.newLine();
+        }
+        fileReader.close();
+        fileWriter.close();
+    }
+}
